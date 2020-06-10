3f1bbdbbc4a479f94b843b89b2fd453c00eec1c2
tsuyama <hirokazuqaz@gmail.com>
2020-06-10 21:16:12 +0000

    Search Engine

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
                                    dependent:   :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+  scope :search_by_keyword, -> (keyword) {
+    where("users.name LIKE :keyword", keyword: "%#{sanitize_sql_like(keyword)}%") if keyword.present?
+  }
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
